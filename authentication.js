acb066334d7cb2c89af2b0e1acb409ad61e7cb03
Kiera <kwalton@liveperson.com>
2022-03-08 09:55:06 +0000

    small changes

--- authentication.js
+++ authentication.js
@@ -9,3 +9,21 @@ function lpGetAuthenticationToken(callback) {
     };
     r.send();
 }
+
+// var tokenCalls = 0;
+
+// function LPJsMethodName(callback) {
+//     addAlert('container', '[2] ' + status + ' lpGetAuthenticationToken(callback)');
+
+//     //Standard
+//     callback('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDY2NjM5MzksImV4cCI6MTY0NjY2NDE3OSwiaXNzIjoicG9jYXBwIiwic3ViIjoiV2ViTXNnVGVzdDEyNDUifQ.dSROepmL3DMw7yDBkbztHYUR7y9XBq8AD4yrRKNCrAke6jwbLCqW_ZZIPLL4OtF-Rrq-4TT9PAlywZ_OVSlvaaaqEPOe1W3durYSQE0sEwQcKNA-Ee3HykQnZ3soxMzDxcPxR8y5ANw5Lm-E9Q67pzZD1lOhWafcrVenX0kpdP_P9NdKK6Vkv8tsuXCCiwUsar1tA36uSggicti65prQiex_qyNZHHgJsvT-uIOs2OzTfQl_EwmCAQM25_1bRygHe06Sj9g2Q4prpJE1-ZjLuosWy0uOwvqJFqUTDkibcQfOfJ22IrdAeH8VTvmD04Xh0wi1ZGmSU6Amdo2o0C07lQ');
+// }
+
+// function addAlert(targetDiv, text) {
+//     var div = document.getElementById(targetDiv);
+//     var alertDiv = document.createElement("div");
+
+//     alertDiv.className = 'alert alert-primary';
+//     alertDiv.innerHTML += text;
+//     div.appendChild(alertDiv);
+// }
