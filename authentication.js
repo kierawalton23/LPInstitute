af8248bd0cb38dade444af6396c2ad1738781711
Kiera <kwalton@liveperson.com>
2022-03-11 11:24:44 +0000

    changes for testing

--- authentication.js
+++ authentication.js
@@ -1,6 +1,6 @@
 function lpGetAuthenticationToken(callback) {
     var r = new XMLHttpRequest();
-    r.open("GET", "https://calm-mesa-48745.herokuapp.com/messaging/idp/token?sub=WebMsgTest1289&expires=259200&passSdes=true");
+    r.open("GET", "https://calm-mesa-48745.herokuapp.com/messaging/idp/token?sub=WebMsgTest1289&expires=259200");
     //&passSdes=true&companyBranch=branch1&firstname=kiera&lastname=walton
     r.onload = function () {
         if (r.status != 200) {
